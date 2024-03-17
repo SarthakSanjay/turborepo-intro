d32485732b31c746b012ee39006e6e0209051be3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-03-17 22:28:14 +0530

    added a react app

---
+++ apps/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import {Button} from "@repo/ui/button"
+function App() {
+  return (
+    <>
+     <Button>hekko</Button>
+    </>
+  )
+}
+
+export default App
